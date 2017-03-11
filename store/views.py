123f0378a71cd723e9cedb7cc748854651df64fc
Slava <horlet@gmail.com>
2017-03-12 02:47:37 +0300

    users isolated

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
+
 from rest_framework import permissions, viewsets
+from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope, TokenHasScope
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -7,8 +9,6 @@ from store.models import StoreItem
 from store.serializers import StoreItemSerializer, UserSerializer
 from store.permissions import IsOwnerOrReadOnly
 
-
-# from rest_framework.pagination import PageNumberPagination
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -23,18 +23,22 @@ class ItemsViewSet(viewsets.ModelViewSet):
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
     """
-    queryset = StoreItem.objects.all()
-    serializer_class = StoreItemSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
+    queryset = StoreItem.objects.all()
+    serializer_class = StoreItemSerializer
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides `list` and `detail` actions.
     """
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(username=self.request.user)
